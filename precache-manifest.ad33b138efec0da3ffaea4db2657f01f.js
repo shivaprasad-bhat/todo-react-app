89fcdfeeaa9707ecaba531fb00e609921f17b901
Shivaprasad <shivaprasad.bhat@learner.manipal.edu>
2020-05-10 13:42:24 +0530

    Updates

---
+++ precache-manifest.ad33b138efec0da3ffaea4db2657f01f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32974dcbfb47b93f459befbd59e9997",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "766f20f5e36b9776ff3e",
+    "url": "/todo/static/css/main.6a0e0ac3.chunk.css"
+  },
+  {
+    "revision": "996cc92a20a563e153ec",
+    "url": "/todo/static/js/2.47b1a3ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.47b1a3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766f20f5e36b9776ff3e",
+    "url": "/todo/static/js/main.844c554b.chunk.js"
+  },
+  {
+    "revision": "1e11d7e4862db0bfe2cb",
+    "url": "/todo/static/js/runtime-main.2c15169a.js"
+  }
+]);
