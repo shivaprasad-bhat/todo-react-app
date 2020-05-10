d04b2370d38f5363d54c69cde7059bd95d8fba68
Shivaprasad <shivaprasad.bhat@learner.manipal.edu>
2020-05-10 14:10:06 +0530

    added changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,6 +8,12 @@ function About() {
                 This is live todo app created with React.
                 <br />
                 Version: 1.0.0
+                <br />
+                Created By:{' '}
+                <a href="https://github.com/shvaprasad-bhat">
+                    Shivaprasad Bhat
+                </a>
+                Courtesy: Traversy Media
             </p>
         </React.Fragment>
     );
